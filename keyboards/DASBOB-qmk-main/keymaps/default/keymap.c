654d9d710c369292e0daf228278172655211993b
FelixCarrier <129521661+Padaf3t@users.noreply.github.com>
2024-05-23 10:20:01 -0400

    keymap base for sym, number and fun

--- keyboards/DASBOB-qmk-main/keymaps/default/keymap.c
+++ keyboards/DASBOB-qmk-main/keymaps/default/keymap.c
@@ -238,24 +238,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_SYM] = LAYOUT_split_3x5_3(
-        KC_ESC,   KC_UP,           XXXXXXX,         XXXXXXX,         CK_TOGG,             KC_NUM_LOCK,  KC_KP_7,          KC_KP_8,         KC_KP_9,           KC_KP_MINUS,
-        KC_LEFT,  RALT_T(KC_DOWN), LCTL_T(KC_RGHT), LSFT_T(XXXXXXX), KC_LGUI,             XXXXXXX,      LSFT_T(KC_KP_4),  LCTL_T(KC_KP_5), RALT_T(KC_KP_6),   KC_KP_PLUS,
-        KC_MPRV,  KC_MSTP,         KC_MPLY,         KC_MNXT,         KC_PSCR,             XXXXXXX,      KC_KP_1,          KC_KP_2,         KC_KP_3,           KC_KP_0,
-                                   KC_TRNS,         TG(0),         KC_TAB,              KC_ENT,       TG(4),          KC_TRNS
+        XXXXXXX,    XXXXXXX,   XXXXXXX,         XXXXXXX,         XXXXXXX,            XXXXXXX,       XXXXXXX,           XXXXXXX,           XXXXXXX,        XXXXXXX,
+        XXXXXXX,    XXXXXXX,   XXXXXXX,         XXXXXXX,         XXXXXXX,            XXXXXXX,       XXXXXXX,           XXXXXXX,           XXXXXXX,        XXXXXXX,
+        XXXXXXX,    XXXXXXX,   XXXXXXX,         XXXXXXX,         XXXXXXX,            XXXXXXX,       XXXXXXX,           XXXXXXX,           XXXXXXX,        XXXXXXX,
+                               KC_RALT,         TG(0),           OSM(MOD_LSFT),      OSM(MOD_LCTL), TG(4),             KC_BSPC
     ),
 
     [_NUMBER] = LAYOUT_split_3x5_3(
-       KC_1,     KC_2,              KC_3,            KC_4,            KC_5,               KC_6,         KC_7,             KC_8,            KC_9,              KC_0,
-       CK_RST,  RALT_T(XXXXXXX),   LCTL_T(XXXXXXX), LSFT_T(XXXXXXX), KC_PSCR,           KC_MINS,      LSFT_T(KC_EQL),   LCTL_T(KC_GRV),  RALT_T(KC_QUOT),    KC_BSLS,
-       CK_TOGG,  CK_UP,             CK_DOWN,          LCTL(KC_C),      LCTL(KC_V),         KC_TAB,      XXXXXXX,          XXXXXXX,         XXXXXXX,           XXXXXXX,
-                                    KC_TRNS,        TG(0),         KC_TAB,             KC_ENT,       TG(5),          KC_TRNS
+       XXXXXXX,     XXXXXXX,   XXXXXXX,        CA_8,            XXXXXXX,            XXXXXXX,        CA_9,              XXXXXXX,           XXXXXXX,         XXXXXXX,
+       CA_6,        CA_4,      CA_2,           CA_0,            XXXXXXX,            XXXXXXX,        CA_1,              CA_3,              CA_5,            CA_7,
+       XXXXXXX,     XXXXXXX,   XXXXXXX,        XXXXXXX,         XXXXXXX,            XXXXXXX,        XXXXXXX,           XXXXXXX,           XXXXXXX,         XXXXXXX,
+                               KC_RALT,        TG(0),           KC_LGUI,            OSM(MOD_LCTL),  TG(5),             KC_BSPC
     ),
 
     [_FUN] = LAYOUT_split_3x5_3(
-        KC_ESC,   KC_UP,           XXXXXXX,         XXXXXXX,         CK_TOGG,             KC_NUM_LOCK,  KC_KP_7,          KC_KP_8,         KC_KP_9,           KC_KP_MINUS,
-        KC_LEFT,  RALT_T(KC_DOWN), LCTL_T(KC_RGHT), LSFT_T(XXXXXXX), KC_LGUI,             XXXXXXX,      LSFT_T(KC_KP_4),  LCTL_T(KC_KP_5), RALT_T(KC_KP_6),   KC_KP_PLUS,
-        KC_MPRV,  KC_MSTP,         KC_MPLY,         KC_MNXT,         KC_PSCR,             XXXXXXX,      KC_KP_1,          KC_KP_2,         KC_KP_3,           KC_KP_0,
-                                   KC_TRNS,         TG(0),         KC_TAB,              KC_ENT,       TG(3),          KC_TRNS
+        XXXXXXX,    XXXXXXX,   XXXXXXX,         XXXXXXX,         XXXXXXX,            XXXXXXX,       XXXXXXX,           XXXXXXX,           XXXXXXX,        XXXXXXX,
+        XXXXXXX,    XXXXXXX,   XXXXXXX,         XXXXXXX,         XXXXXXX,            XXXXXXX,       XXXXXXX,           XXXXXXX,           XXXXXXX,        XXXXXXX,
+        XXXXXXX,    XXXXXXX,   XXXXXXX,         XXXXXXX,         XXXXXXX,            XXXXXXX,       XXXXXXX,           XXXXXXX,           XXXXXXX,        XXXXXXX,
+                               KC_RALT,         TG(0),           KC_LGUI,            OSM(MOD_LCTL), TG(3),             KC_BSPC
     )
 
 
